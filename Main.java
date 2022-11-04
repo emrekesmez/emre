0115e8669a03bf0dc07d37f04c2e488b04f3ab6c
emrekesmez <107947518+emrekesmez@users.noreply.github.com>
2022-11-04 17:10:23 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,25 +2,12 @@ import java.util.Scanner ;
 
 public class Main {
     public static void main(String[] args) {
-        int mat, fizik, kimya, turkce, tarih, muzik ;
-        Scanner input = new Scanner(System.in);
-
-        System.out.print ("matematik notunu giriniz:");
-        mat = input.nextInt();
-        System.out.print("fizik notunu giriniz:");
-        fizik = input.nextInt();
-        System.out.print("kimya notunu giriniz:");
-        kimya = input.nextInt();
-        System.out.print("türkçe notunu giriniz:");
-        turkce = input.nextInt();
-        System.out.print("tarih notunu giriniz:");
-        tarih = input.nextInt();
-        System.out.print("müzik notunu giriniz:");
-        muzik = input.nextInt();
-        double sonuc = (mat + fizik + kimya + turkce + tarih + muzik) ;
-        double bitis = (sonuc / 6) ;
-        boolean gecme = bitis >= 60 ;
-        System.out.println(bitis);
-        System.out.println(gecme ? "Geçti" : "kaldı");
+        double fiyat , kdv , sonuc;
+        Scanner input = new Scanner(System.in) ;
+        System.out.println("fiyat bilgisini giriniz:");
+        fiyat = input.nextDouble() ;
+        kdv = (fiyat * 118) ;
+        sonuc = (kdv / 100) ;
+        System.out.println(sonuc);
     }
 }
