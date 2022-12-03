cd0bf397e9c8de33e761672782a9879008bf9d31
emrekesmez <107947518+emrekesmez@users.noreply.github.com>
2022-12-03 15:46:24 +0300

    Add files via upload

---
+++ mükemmelsayı.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class mükemmelsayı {
+    public static void main(String[] args) {
+
+        int sayi, toplam = 0;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayıyı giriniz :");
+        sayi = input.nextInt();
+
+        for (int i = 1; i < sayi; i++) {
+            if (sayi % i == 0) {
+                toplam += i;
+            }
+        }
+
+        if (sayi == toplam) {
+            System.out.println(sayi + "bir mükemmel sayıdır. ");
+        } else {
+            System.out.println(sayi + "mükemmel sayı değildir.");
+        }
+    }
+}
